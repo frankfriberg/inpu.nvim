e7923b8bd5adadcb30906cbf0137136e3a1e4b1c
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-03-03 12:55:06 +0100

    feat: take into account when user is pasting

--- lua/inpu.lua
+++ lua/inpu.lua
@@ -19,12 +19,14 @@ end
 function M.input(opts, on_confirm)
 	local prompt = opts.prompt and string.format(" %s", opts.prompt) or " Input: "
 	local default = opts.default or ""
+	local win_config
+	local is_centered = not (prompt == " New Name: ")
+
 	on_confirm = on_confirm or function() end
 
 	local min_width = M.config.min_width
 	local dynamic_width = M.config.dynamic_width
 
-	-- Initial input width calculation
 	local function calculate_width()
 		local default_width = vim.str_utfindex(default) + 10
 		local prompt_width = vim.str_utfindex(prompt) + 10
@@ -43,38 +45,38 @@ function M.input(opts, on_confirm)
 		title = prompt,
 	}
 
-	-- Apply user's window config.
-	local win_config = vim.tbl_deep_extend("force", default_win_config, M.window_center(M.config.min_width))
-
-	-- Position the window if rename
-	if prompt == " New Name: " then
-		win_config = vim.tbl_deep_extend("force", win_config, M.under_cursor(win_config.width))
+	if is_centered then
+		win_config = vim.tbl_deep_extend("force", default_win_config, M.window_center(M.config.min_width))
+	else
+		win_config = vim.tbl_deep_extend("force", default_win_config, M.under_cursor(win_config.width))
 	end
 
-	-- Create floating window
 	local buffer = vim.api.nvim_create_buf(false, true)
 	local window = vim.api.nvim_open_win(buffer, true, win_config)
 	vim.api.nvim_buf_set_text(buffer, 0, 0, 0, 0, { default })
 
-	-- Put cursor at the end of the default value
 	vim.cmd("startinsert")
 	vim.api.nvim_win_set_cursor(window, { 1, vim.str_utfindex(default) + 1 })
 
-	-- Adjust width dynamically as user types
 	if dynamic_width then
-		vim.api.nvim_create_autocmd("TextChangedI", {
+		vim.api.nvim_create_autocmd({ "TextChangedI", "TextChangedP" }, {
 			buffer = buffer,
 			callback = function()
 				if vim.api.nvim_win_is_valid(window) then
 					local lines = vim.api.nvim_buf_get_lines(buffer, 0, 1, false)
 					local new_width = math.max(min_width, vim.str_utfindex(lines[1] or "") + 5)
-					vim.api.nvim_win_set_config(window, { width = new_width })
+
+					if is_centered then
+						local new_col = (vim.api.nvim_win_get_width(0) - new_width) / 2
+						vim.api.nvim_win_set_config(window, { width = new_width, col = new_col })
+					else
+						vim.api.nvim_win_set_config(window, { width = new_width })
+					end
 				end
 			end,
 		})
 	end
 
-	-- Enter to confirm
 	vim.keymap.set({ "n", "i", "v" }, "<cr>", function()
 		if vim.api.nvim_win_is_valid(window) then
 			local lines = vim.api.nvim_buf_get_lines(buffer, 0, 1, false)
@@ -84,7 +86,6 @@ function M.input(opts, on_confirm)
 		end
 	end, { buffer = buffer })
 
-	-- Esc or q to close
 	local function close_window()
 		if vim.api.nvim_win_is_valid(window) then
 			on_confirm(nil)
